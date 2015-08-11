03c81797f9b19d64e8fa97b7989c9b4ebddf45cd
jrdn hannah <jrdn@jrdnhannah.co.uk>
2015-08-11 16:12:28 +0100

    [Compiler] Add unit test for event listeners compiler pass

---
+++ tests/Bundle/Compiler/LoadEventListenersTest.php
@@ -0,0 +1,51 @@
+<?php namespace SmoothPhp\SymfonyBridge\Test\Bundle\Compiler;
+
+use ReflectionProperty;
+use PHPUnit_Framework_TestCase as TestCase;
+use SmoothPhp\EventDispatcher\SimpleEventDispatcher;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use SmoothPhp\SymfonyBridge\Bundle\Compiler\LoadEventListeners;
+use SmoothPhp\SymfonyBridge\Test\Stub\EventDispatcher\ListenerStub;
+
+/**
+ * Class LoadEventListenersTest
+ *
+ * @author jrdn hannah <jrdn@jrdnhannah.co.uk>
+ */
+final class LoadEventListenersTest extends TestCase
+{
+    /** @var ContainerBuilder */
+    private $containerBuilder;
+
+    protected function setUp()
+    {
+        $this->containerBuilder = new ContainerBuilder;
+        $this->containerBuilder->setDefinition(
+            'smooth_php.event_dispatcher',
+            new Definition(SimpleEventDispatcher::class)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_add_handlers_to_the_resolver_store()
+    {
+        $pass = new LoadEventListeners;
+
+        $listener = new Definition(ListenerStub::class);
+        $listener->addTag('smooth_php.event_listener', ['method' => 'onEventDispatched', 'event' => 'foo.bar']);
+
+        $this->containerBuilder->setDefinition('foo.bar', $listener);
+
+        $pass->process($this->containerBuilder);
+
+        $resolver = $this->containerBuilder->get('smooth_php.event_dispatcher');
+
+        $refl = new ReflectionProperty(SimpleEventDispatcher::class, 'listeners');
+        $refl->setAccessible(true);
+
+        $this->assertSame(1, count($refl->getValue($resolver)));
+    }
+}
---
+++ tests/Stub/EventDispatcher/ListenerStub.php
@@ -0,0 +1,12 @@
+<?php namespace SmoothPhp\SymfonyBridge\Test\Stub\EventDispatcher;
+
+/**
+ * Class ListenerStub
+ *
+ * @author jrdn hannah <jrdn@jrdnhannah.co.uk>
+ */
+final class ListenerStub
+{
+    public function onEventDispatched()
+    {}
+}
